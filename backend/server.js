f69aceae55b15f625cfe1a762d5742cf02591247
Priya <priya1@mt.iit.ac.in>
2026-06-13 11:04:54 +0530

    Day 2 - Socket.io room joining

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,29 @@
-function App() {
-  return (
-    <div>
-      <h1>P2P Web Share</h1>
-      <p>Day 1 - Signaling Server Connected</p>
-    </div>
-  );
-}
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-export default App;
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    console.log(`${socket.id} joined room ${roomId}`);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Signaling server running on port 5000");
+});
